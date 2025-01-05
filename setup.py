0d72b45f6ec50a244455aed6eee8301d3a2e0a20
astro-jingtao <634790985@qq.com>
2025-01-05 16:11:03 +0800

    update description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name="cmap_factory",
       version="0.0.1",
       author="Tao Jing",
       author_email="jingt20@mails.tsinghua.edu.cn",
-      description="Generate color maps",
+      description="Generate color maps from images, color palettes, or other sources.",
       packages=find_packages(),
       install_requires=[
           'networkx', 'numpy', 'matplotlib', 'scipy', 'scikit-image',
